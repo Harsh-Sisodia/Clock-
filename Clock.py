d0f184424b6c44da6d6be896bd2b4b24e2b2fb03
Harsh-Sisodia <harshvardhan.24jiaiml050@jietjodhpur.ac.in>
2024-11-10 16:43:15 +0530

    Clock

---
+++ Clock.py
@@ -0,0 +1,13 @@
+import datetime as dt
+import time
+import os
+while True:
+    now = dt.datetime.now()
+    current_time = now.strftime("%I:%M:%S %p")
+    current_time2= now.strftime("%H:%M:%S")
+    current_time3= now.strftime("%B %d, %Y")
+    print(current_time)
+    print(current_time2)
+    print(current_time3)
+    time.sleep(1)
+    os.system('clear')
